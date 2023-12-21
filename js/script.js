ffe6c4d8e8475fdb421be8f51f4bcbe92826c87b
ryuzakiaoi <sleepyash467@gmail.com>
2023-12-21 13:59:00 +0700

    firs commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hambuger Menu di Klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik Di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
